e5f2d995d6e3a523f2519544e13939abe02133b2
Bardia Doust <bardiad@gmail.com>
2025-07-13 18:56:55 -0400

    added img optimization and js minification

--- gulp/tasks/imgs.js
+++ gulp/tasks/imgs.js
@@ -2,18 +2,21 @@ import { src, dest } from 'gulp';
 import imagemin from 'gulp-imagemin';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import newer from 'gulp-newer';
 import gulpif from "gulp-if";
 import * as util from './common.js';
-
 
 const config   = util.getConfig('gulp.config.json');
 const argv = yargs(hideBin(process.argv)).argv;
 
 function _optimizeImgs(callback) {
+    const srcPath = global.SOURCES_BASE_PATH + '/img/**/*';
+    const destPath = global.BASE_PATH + '/dist/img';
 
-    return src( global.SOURCES_BASE_PATH + '/img/**/*', {encoding: false} )
-    .pipe( gulpif(argv.prod, imagemin()) )
-    .pipe( dest( global.BASE_PATH + "/dist/img" ) )
+    return src(srcPath, { encoding: false })
+    .pipe(newer(destPath)) 
+    .pipe(gulpif(argv.prod, imagemin()))
+    .pipe(dest(destPath));
 }
 
 export { _optimizeImgs as optimizeImg };
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,19 +1,19 @@
-import { src, dest, series } from 'gulp';
-import fs from 'fs';
-
-
+// scripts.js
+import { src, dest } from 'gulp';
 import * as util from './common.js';
+import gulpif from 'gulp-if';
+import terser from 'gulp-terser';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 const config = util.getConfig('gulp.config.json');
+const argv = yargs(hideBin(process.argv)).argv;
 
 function buildScripts(callback) {
-
-
-  return src([
-    globalThis.SOURCES_BASE_PATH + '/js/**/*.js'
-  ])
+  return src([globalThis.SOURCES_BASE_PATH + '/js/**/*.js'])
+    .pipe(gulpif(argv.prod, terser())) 
     .pipe(dest(globalThis.BASE_PATH + '/dist/js'))
     .pipe(globalThis.CONNECT.reload());
 }
 
-// ESM Exports
 export { buildScripts };
